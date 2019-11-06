b36e5f960c7bdfae89e1d572f0bb21d69bacf338
Guillaume Duhan <guillaume.duhan@quatreepingles.fr>
2019-11-06 12:59:10 +0100

    contributors part added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,14 @@
 <template lang="pug">
 #Home
-  img(src='https://myhappyagency.com/blog/wp-content/uploads/2019/10/vue-bootstrap-vuex-eslint.png')
-  h1 {{ pjson.name }}
-  p {{ pjson.description }}
+  header
+    img(src='https://myhappyagency.com/blog/wp-content/uploads/2019/10/vue-bootstrap-vuex-eslint.png')
+    h1 {{ pjson.name }}
+    p {{ pjson.description }}
+  .container.d-flex.flex-column.justify-content-center.align-items-center
+    p Contributors:
+    .avatar(v-for="contributor, index in contributors")
+      a(:href="contributor.link", target="_blank")
+        img(:src="contributor.img")
 </template>
 
 <script>
@@ -13,9 +19,25 @@ export default {
   data() {
     return {
       pjson,
+      contributors: [
+        {
+          link: 'https://github.com/guillaumeduhan',
+          img: 'https://avatars1.githubusercontent.com/u/31253241?s=460&v=4',
+        },
+      ],
     }
   },
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+#Home {
+  .avatar {
+    img {
+      border-radius: 75px;
+      max-width: 75px;
+      margin-bottom: 15px;
+    }
+  }
+}
+</style>
