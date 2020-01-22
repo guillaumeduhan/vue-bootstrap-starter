49c6741e470b8ea50e07e669b08efe9e2000dff1
halma <myhappyagency@gmail.com>
2020-01-22 13:17:45 +0100

    Home update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,11 @@
 #Home
   header
     img(src='https://myhappyagency.com/blog/wp-content/uploads/2019/10/vue-bootstrap-vuex-eslint.png')
-    h1 {{ pjson.name }}
-    p {{ pjson.description }}
-    a(href="https://github.com/guillaumeduhan/vue-bootstrap-boilerplate", target="_blank") Github
 </template>
 
 <script>
-import pjson from '../../package.json'
-
 export default {
-  name: 'Home',
-  data() {
-    return {
-      pjson,
-    }
-  },
+  name: 'Home'
 }
 </script>
 
