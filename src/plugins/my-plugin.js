7c3ffe2c1ad977044f9c4f5bd0d5de54311ffcf6
Guillaume Duhan <myhappyagency@gmail.com>
2020-01-26 22:34:15 +0100

    myPlugin

---
+++ src/plugins/my-plugin.js
@@ -0,0 +1,17 @@
+// This is your plugin object. It can be exported to be used anywhere.
+const MyPlugin = {
+  // The install method is all that needs to exist on the plugin object.
+  // It takes the global Vue object as well as user-defined options.
+  install(Vue, options) {
+    // We call Vue.mixin() here to inject functionality into all components.
+  	Vue.mixin({
+      // Anything added to a mixin will be injected into all components.
+      // In this case, the mounted() method runs when the component is added to the DOM.
+      mounted() {
+        console.log('Mounted!')
+      },
+    })
+  },
+}
+
+export default MyPlugin
