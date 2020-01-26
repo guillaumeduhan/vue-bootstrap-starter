85b7e525c5819bc1ae9573770d744d6d1c38b756
Guillaume Duhan <myhappyagency@gmail.com>
2020-01-26 22:48:05 +0100

    update

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import MyPlugin from './plugins/my-plugin.js'
+import MyPlugin from './plugins/my-plugin'
 
 Vue.use(BootstrapVue)
 Vue.use(MyPlugin)
