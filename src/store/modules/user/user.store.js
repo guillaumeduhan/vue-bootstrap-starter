8018677e078c7cd5d147b5353c1f9cf32c095ea8
Guillaume Duhan <myhappyagency@gmail.com>
2020-04-08 12:14:36 +0200

    user.store.js fix

--- src/store/modules/user/user.store.js
+++ src/store/modules/user/user.store.js
@@ -1,18 +1,17 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import actions from './actions'
-import mutations from './mutations'
-import getters from './getters'
-import state from './state'
+import Vue from "vue";
+import Vuex from "vuex";
+import actions from "./actions";
+import mutations from "./mutations";
+import getters from "./getters";
+import state from "./state";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-const userStore = new Vuex.Store({
+const userStore = {
   actions,
   mutations,
   getters,
   state,
-  strict: process.env.NODE_ENV !== 'production',
-})
+};
 
-export default userStore
+export default userStore;
