4f8d6f7c088fa3503698031146af0d34720c07c0
Guillaume Duhan <myhappyagency@gmail.com>
2020-06-19 22:39:27 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2C3E50;
+  color: #2c3e50;
   min-height: 100vh;
 
   .container {
