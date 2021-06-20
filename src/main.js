c98bf684f707d356917dbf2253c539535315c7fe
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2021-06-20 18:03:26 +0100

    fix: remove plugin useless code

--- src/main.js
+++ src/main.js
@@ -4,10 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import MyPlugin from './plugins/my-plugin'
-
 Vue.use(BootstrapVue)
-Vue.use(MyPlugin)
 
 Vue.config.productionTip = false
 Vue.prototype.$log = console.log
